0b6099e70be4ed5544a6ec710bff44b4ce11cc7f
Felix Ghislain <felix@C02FLCE0MD6M.hitronhub.home>
2022-02-23 20:08:04 +0000

    navbar url link update

--- shortenFrontend/src/components/navBar/Navbar.tsx
+++ shortenFrontend/src/components/navBar/Navbar.tsx
@@ -6,7 +6,9 @@ export const Navbar = () => {
       <div className="max-w-5xl mx-auto py-12">
         <div className="flex justify-between items-center">
           <div className="font-Satisfy text-2xl">Shorten</div>
-          <div>GitHub</div>
+          <div>
+            <a href="https://github.com/felixghislaindev/shorten">GitHub</a>
+          </div>
         </div>
       </div>
     </nav>
